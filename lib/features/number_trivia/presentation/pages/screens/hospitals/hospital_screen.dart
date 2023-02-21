7a7779d727a6ca402a8265f14ab3fa9531eea12a
Olvine23 <olivinegeorge@gmail.com>
2023-02-21 23:42:15 +0300

    Implemented geolocation

--- lib/features/number_trivia/presentation/pages/screens/hospitals/hospital_screen.dart
+++ lib/features/number_trivia/presentation/pages/screens/hospitals/hospital_screen.dart
@@ -1,8 +1,12 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 import 'package:geolocator/geolocator.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:test/core/styles/styles.dart';
+import 'package:test/core/utils/colors.dart';
 import 'package:test/features/number_trivia/presentation/widgets/search_input.dart';
+import 'package:http/http.dart' as http;
 
 class HospitalScreen extends StatefulWidget {
   @override
@@ -10,8 +14,13 @@ class HospitalScreen extends StatefulWidget {
 }
 
 class _HospitalScreenState extends State<HospitalScreen> {
+  List<dynamic> hospitals = [];
+  Map<MarkerId, Marker> markers = <MarkerId, Marker>{};
   String? _currentAddress;
   Position? _currentPosition;
+  LatLng? _center;
+
+  late Position currentLocation;
   GoogleMapController? _mapController;
   final Set<Marker> _markers = {};
 
@@ -19,6 +28,46 @@ class _HospitalScreenState extends State<HospitalScreen> {
 
   static final CameraPosition _kInitialPosition =
       CameraPosition(target: _kMapCenter, zoom: 11.0, tilt: 0, bearing: 0);
+
+  Future<void> _getLocation() async {
+    currentLocation = await Geolocator.getCurrentPosition(
+        desiredAccuracy: LocationAccuracy.high);
+    setState(() {
+      _center = LatLng(currentLocation.latitude, currentLocation.longitude);
+    });
+  }
+
+  Future<void> _getHospitals() async {
+    print(_center!.latitude);
+    print(_center!.longitude);
+    String url =
+        'https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=${_center!.latitude},${_center!.longitude}&radius=15000&type=hospital&key=AIzaSyCYinXlTBVd27JW6sHe3xixPmOfNPTdo7E';
+    var response = await http.get(Uri.parse(url));
+    Map<String, dynamic> data = json.decode(response.body);
+    setState(() {
+      hospitals = data['results'];
+    });
+    _addMarkers();
+  }
+
+  void _addMarkers() {
+    hospitals.asMap().forEach((index, hospital) {
+      var markerIdVal = index.toString();
+      final MarkerId markerId = MarkerId(markerIdVal);
+      final Marker marker = Marker(
+        markerId: markerId,
+        position: LatLng(
+          hospital['geometry']['location']['lat'],
+          hospital['geometry']['location']['lng'],
+        ),
+        infoWindow: InfoWindow(
+          title: hospital['name'],
+          snippet: hospital['vicinity'],
+        ),
+      );
+      markers[markerId] = marker;
+    });
+  }
 
   Future<bool> _handleLocationPermission() async {
     bool serviceEnabled;
@@ -56,9 +105,10 @@ class _HospitalScreenState extends State<HospitalScreen> {
     setState(() {
       _currentPosition = position;
       _markers.add(Marker(
-          markerId: MarkerId("Your Location"),
+          markerId: MarkerId("Your Location",), 
           position: LatLng(position.latitude, position.longitude),
           infoWindow: InfoWindow(title: "Your Location")));
+          
     });
   }
 
@@ -76,7 +126,7 @@ class _HospitalScreenState extends State<HospitalScreen> {
   @override
   void initState() {
     super.initState();
-    _getCurrentLocation();
+    _getLocation().then((value) => _getHospitals());
   }
 
   @override
@@ -96,19 +146,16 @@ class _HospitalScreenState extends State<HospitalScreen> {
             text: "Search for hospitals",
           ),
           SizedBox(
-            height: 90,
+            height: 20,
           ),
           Expanded(
             child: Container(
               height: 400,
               child: GoogleMap(
-                  initialCameraPosition: _kInitialPosition,
-                  myLocationEnabled: true,
-                  markers: {
-                    Marker(
-                        markerId: MarkerId('My Location'),
-                        position: _kMapCenter),
-                  }),
+                initialCameraPosition: _kInitialPosition,
+                myLocationEnabled: true,
+                markers: Set<Marker>.of(markers.values),
+              ),
             ),
           ),
         ],
